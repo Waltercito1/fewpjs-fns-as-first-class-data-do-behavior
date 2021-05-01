f04f674b3906f46bb9a3eb605e662b5fd59a8c9a
Walter Aab <aabwalter@yahoo.com>
2021-05-01 14:32:55 -0500

    commit tree 5c64868b2febdd823df891ab8799aa866f90d532

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10);
+  if (hour < 12 ) return "Good Morning"
+  if (hour > 17 ) return "Good Evening"
+  return "Good Afternoon"
+}
+
+/* End Given Code, don't edit above here...*/
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
+
+
